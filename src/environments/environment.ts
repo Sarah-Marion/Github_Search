362509ca59076737b591f1b043a6d05387d992b6
Sarah-Marion <devsarahmarion@gmail.com>
2018-04-29 14:14:12 +0300

    placed access tokens and authorization key within

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,8 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiRoot: "https:api.github.com/",
+  Authorization: '277a9bcde8c6960dea4b11b795ba060e70f41070',
+  access_token: '277a9bcde8c6960dea4b11b795ba060e70f41070'
 };
