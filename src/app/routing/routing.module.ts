92b27296501d7b55cc960d8fd15df236179a4a2c
Sarah-Marion <devsarahmarion@gmail.com>
2018-04-29 14:16:21 +0300

    set up different route configurations

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterModule,Routes} from '@angular/router';
+
+import { HomeComponent } from '../home/home.component';
+import { NotFoundComponent } from '../not-found/not-found.component';
+
+const routes:Routes=[
+  {path:"home",component:HomeComponent},
+  {path:"",redirectTo:"/home",pathMatch:"full"},
+  {path:'**',component:NotFoundComponent}
+]
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
   ],
   declarations: []
 })
