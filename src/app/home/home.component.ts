1e74606b4208134682252bcd781aae88d9960331
Sarah-Marion <devsarahmarion@gmail.com>
2018-04-29 14:22:34 +0300

    called relative imports and gave working functionality to the app

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ApiRequestService } from '../http/api-request.service';
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  userProfile: any = [];
+  repos: any = [];
+  username: string ;
+  public loading: boolean = false;
+  public message_status: boolean = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  receiveDate(get){
+  get.timeCreated = new Date (get.timeCreated);
+  this.userProfile.push(get);
   }
 
-}
+  constructor(private ApiRequestService: ApiRequestService,public AS: ApiRequestService, private http: HttpClient) {
+    this.message_status = this.AS.message_status;
+   }
+   doSearch(term: string){
+     term= term || "Sarah-Marion"
+     this.AS.results = [];
+     this.AS.results1 = [];
+     this.AS.results2 = [];
+     this.loading = true;
+     this.AS.search(term).then(() => this.loading = false)
+     return false;
+   }
+
+  ngOnInit() {
+    this.doSearch("Sarah-Marion");
+    this.ApiRequestService.getUserProfile().subscribe(users => {
+      this.userProfile = users;
+    });
+
+    this.ApiRequestService.getUserRepos().subscribe(repos => {
+      this.repos = repos;
+    });
+  }
+  searchUserProfile() {
+    this.ApiRequestService.updateUserProfile(this.username);
+
+    this.ApiRequestService.getUserProfile().subscribe(users => {
+      this.userProfile = users;
+    });
+
+    this.ApiRequestService.getUserRepos().subscribe(repos => {
+      this.repos = repos;
+    });
+
+ }}
