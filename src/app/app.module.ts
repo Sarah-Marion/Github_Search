6350e8ce46813df8cb43e4203828c4f4e203c78b
Sarah-Marion <devsarahmarion@gmail.com>
2018-04-29 14:12:33 +0300

    added importation of relevant Modules for functionality of the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ApiRequestService } from './http/api-request.service';
+import { NotFoundComponent } from './not-found/not-found.component';
+import {RoutingModule} from './routing/routing.module';
+import { HighlightDirective } from './highlight.directive';
+import { DateCountPipe } from './date-count.pipe';
 
+const routes:Routes=[
+  {path:"home",component:HomeComponent},
+  {path:"",redirectTo:"/home",pathMatch:"full"},
+  {path:'**',component:NotFoundComponent}
+]
+  
+  
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent,
+    HighlightDirective,
+    DateCountPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RoutingModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [ApiRequestService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
